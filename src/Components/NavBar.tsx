6ae12d0b0ea894cba06cd771a45e8c1391375ca0
Tulsi kalola <66298634+TULSI2012@users.noreply.github.com>
2024-03-11 09:36:13 -0400

    Add product card and some new file

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,28 @@
-const NavBar = () => {
-  return <div></div>;
-};
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default NavBar;
+export default function DenseAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,26 @@
+import ProductCard from "../Components/ProductCard";
+import Candy from "../assets/Candy.jpg";
+import products from "../Data/Product.json";
+import "../Components/Home.css";
+
 const Home = () => {
-  return <div></div>;
+  const productsArray = products.products.data.items;
+  return (
+    <>
+      <div className="container">
+        <div className="cards">
+          {productsArray.map((product) => (
+            <ProductCard
+              image={Candy}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;

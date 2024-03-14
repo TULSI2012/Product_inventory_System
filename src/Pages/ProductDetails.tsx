bb698b30ce47dd77585950321146327bdc57b0ff
Tulsi kalola <66298634+TULSI2012@users.noreply.github.com>
2024-03-14 10:43:15 -0400

    add description

--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
 
 function ProductDetails() {
-  const params = useParams();
-  const id = params.id;
+  const { id } = useParams();
   console.log(id);
-  return <div>Product Screen</div>;
+  return (
+    <div>
+      <img>{product.image}</img>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <button>Favourite</button>
+      <button>Add To Cart</button>
+    </div>
+  );
 }
 export default ProductDetails;
